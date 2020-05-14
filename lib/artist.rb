7b283ec91c83accc7def1d774a5a43b5d84a6bee
Steve Womble <swomble@aol.com>
2020-05-14 22:43:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -29,7 +29,7 @@ class Artist
   end
   
   def self.find_or_create_by_name(name)
-    self.all.detect{|artist|artist.name == name} || Artist.new(name).tap {|new_artist| new_artist.save}
+    self.all.detect{|artist|artist.name == name} || Artist.new(name)
   end
   
   def print_songs
